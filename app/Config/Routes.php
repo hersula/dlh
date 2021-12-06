dbc7f5cb658651c79f8d31f4b93c64de954c37c1
Henry Kurniawan <henrykurniawan1997@gmail.com>
2021-12-06 23:44:19 +0700

    tedst

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,7 +30,7 @@ $routes->setAutoRoute(true);
  */
 
 // We get a performance increase by specifying the default
-// route since we don't have to scan directories.
+// route since we don't have to scan directories...
 $routes->get('/', 'Home::index');
 $routes->get('/admin/dashboard', 'admin/Dashboard::index');
 $routes->get('/user/dashboard', 'user/Dashboard::index');
